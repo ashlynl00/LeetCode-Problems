9d11577f080d57e4b7660c62c481862002a2e277
Ashlyn Lanaville <90938725+ashlynl00@users.noreply.github.com>
2022-04-20 19:35:55 -0700

    Time: 65 ms (71.31%), Space: 41.9 MB (52.88%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let countingFunc = (stairsRemaining, savedResults) => {
+        if (stairsRemaining < 0) {
+            return 0;
+        }
+        if (stairsRemaining == 0) {
+            return 1;
+        }
+        if (savedResults[stairsRemaining]) {
+            return savedResults[stairsRemaining]
+        }
+        savedResults[stairsRemaining] =
+            countingFunc(stairsRemaining - 1, savedResults) + countingFunc(stairsRemaining - 2, savedResults);
+            return savedResults[stairsRemaining];
+    }
+    return countingFunc(n, {})
+};
